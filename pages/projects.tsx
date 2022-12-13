3dd50c43483444fab37680ee5f397b91ddc9ee08
Richard Li <48068570+clsscrch@users.noreply.github.com>
2022-12-13 00:42:13 -0500

    Changed cards and improved mobile view

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -10,25 +10,35 @@ export default function Projects() {
                 <title>Projects</title>
                 <meta name="description" content="My projects consisting of professional, personal, and educational " />
             </Head>
-            {/* <Navbar /> */}
-            <section className='flex flex-col justify-center items-center py-2 '>
-                <h2 className='text-4xl font-bold pt-2 items-center justify-center'>Professional Projects:</h2>
-                {/* <h2 className='text-2xl font-bold'>ECFMG</h2> */}
 
-                <h2 className='text-4xl font-bold pt-2'>
-                    Personal Projects
-                </h2>
-
-                <CardContainer>
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-
-
+            <section className='flex flex-col justify-center items-center py-2 pb-10'>
+                <CardContainer title="Professional Projects">
+                    <Card title="Pathways">
+                        Hello
+                    </Card>
+                    <Card title="Owner Community">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+                    </Card>
                 </CardContainer>
 
-                {/* <h1 className='text-4xl font-bold'>School Projects:</h1> */}
+                <CardContainer title="Personal Projects">
+                    <Card title="Indeed Job Data Analyzer">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+                    </Card>
+                    <Card title="Price Per Wing">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+                    </Card>
+                </CardContainer>
+
+                <CardContainer title="School/Educational Projects">
+                    <Card title="Canny Edge Detector" >
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+                    </Card>
+                    <Card title="Owner Community" >
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+                    </Card>
+                </CardContainer>
+
             </section>
         </>
     )
