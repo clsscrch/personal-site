e070164e71b3ab1a26765f96e0f2edfc26367004
Richard Li <48068570+clsscrch@users.noreply.github.com>
2022-12-13 02:54:38 -0500

    Added an icon with a tool tip component

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,8 @@ import { MoonIcon } from '@heroicons/react/24/solid'
 import { Icon } from '@iconify/react';
 import { useRouter } from 'next/router'
 import { useState } from 'react';
-// import styles from '../styles/Home.module.css'
+import ToolTippedIcon from '../components/ToolToppedIcon';
+import { SiNextdotjs, SiReact, SiTailwindcss, SiTypescript } from "react-icons/si";
 
 export default function Home() {
 
@@ -21,11 +22,22 @@ export default function Home() {
         <Image priority src='/me.png' width={500} height={500} className='rounded-full h-28 w-28 md:h-56 md:w-56' alt='A picture of me, Richard Li, in a red flannel in front of a brick wall' />
         <h1 className='text-xl md:text-5xl font-bold pt-2'>Hi, I&#x27;m Richard</h1>
         <h2 className='text-md md:text-2xl font-bold'>I&#x27;m a software engineer</h2>
-
       </section>
+
       <section className='flex justify-center items-center space-x-10'>
         <a href='https://github.com/clsscrch' target='_blank' rel='noopener noreferrer'><Icon icon="logos:github-icon" width={26} height={26} className='cursor-pointer' /></a>
         <a href='https://www.linkedin.com/in/richard-li-b204aa16b/' target='_blank' rel='noopener noreferrer'><Icon icon="mdi:linkedin" width={32} height={32} /></a>
+      </section>
+
+      <section className='flex flex-col justify-center items-center pt-12'>
+        <p className='text-xl font-bold pb-3'>Built With</p>
+        <ul className='flex items-center space-x-6'>
+          <ToolTippedIcon icon={<SiNextdotjs />} tooltip={'Next.js'} />
+          <ToolTippedIcon icon={<SiReact />} tooltip={'React'} />
+          <ToolTippedIcon icon={<SiTypescript />} tooltip={'TypeScript'} />
+          <ToolTippedIcon icon={<SiTailwindcss />} tooltip={'TailwindCSS'} />
+        </ul>
+
       </section>
 
     </div >
